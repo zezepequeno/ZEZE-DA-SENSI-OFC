726c03e69c62639e7bf171e797067404237f2627
zezepequeno <rafaelaranja90@gmail.com>
2026-01-28 01:50:20 -0300

    Update sensi.js

--- public/js/sensi.js
+++ public/js/sensi.js
@@ -1,11 +1,4 @@
 import { gerarSensiIA } from "./ai-sensi.js";
-
-export function GERAR_SENSI_IA(modelo, vipStatus) {
-  try {
-    // Passa o modelo e se o usuário é VIP para a lógica da IA
-    return gerarSensiIA(modelo, vipStatus);
-  } catch (e) {
-    console.error("Erro ao gerar:", e);
-    return `<p style="color:red;">⚠️ Erro ao calcular sensibilidade.</p>`;
-  }
+export function GERAR_SENSI_IA(modelo, vip) {
+    return gerarSensiIA(modelo, vip);
 }
