f21438ff85dbff113bc790ec3fc00d31a765f1f2
zezepequeno <rafaelaranja90@gmail.com>
2026-01-28 11:39:07 -0300

    Delete public directory

--- public/js/sensi.js
+++
@@ -1,7 +0,0 @@
-import { gerarSensiIA } from "./ai-sensi.js";
-
-export function gerarSensi() {
-    const modelo = document.getElementById("modelo").value;
-    document.getElementById("resultado").innerHTML =
-        gerarSensiIA(modelo, false);
-}
